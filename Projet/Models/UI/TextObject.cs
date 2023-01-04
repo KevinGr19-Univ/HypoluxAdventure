9c7bc216d68dfe0baf1bc32f0a5a1fae9cf8b8a9
Noa GUILLOT <noa.guillot@etu.univ-smb.fr>
2023-01-04 11:28:17 +0100

    CreditScreen update

--- Projet/Models/UI/TextObject.cs
+++ Projet/Models/UI/TextObject.cs
@@ -14,15 +14,19 @@ namespace HypoluxAdventure.Models.UI
         private SpriteFont _font;
         private string _text;
         private SpriteBatch _spriteBatch;
-        public TextObject(SpriteFont font, string text)
+        private Vector2 _position;
+
+        public TextObject(SpriteFont font, string text, Vector2 position)
         {
             _font = font;
             _text = text;
+            _position = position;
         }
 
         public void Draw()
         {
-            Vector2 center = _font.MeasureString(_text);
+            Vector2 measure = _font.MeasureString(_text);
+            _spriteBatch.DrawString(_font, _text, _position - measure*0.5f, Color.White);
 
         }
 
