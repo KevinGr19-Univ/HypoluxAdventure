ee47e8cd80e43b3800e5306cbc96f40611fd3ec7
Noa GUILLOT <noa.guillot@etu.univ-smb.fr>
2023-01-04 12:21:44 +0100

    Changement CreditScreen

--- Projet/Screens/CreditScreen.cs
+++ Projet/Screens/CreditScreen.cs
@@ -17,9 +17,13 @@ namespace HypoluxAdventure.Screens
         private SpriteFont _titleFont;
         private SpriteFont _normalFont;
         private TextObject _title;
-        private TextObject[] _textList = new TextObject[10];
-        public Vector2 startpoint = new Vector2(Application.SCREEN_WIDTH * 0.5f, Application.SCREEN_HEIGHT + 100); 
-        //public float 
+
+        private List<TextObject> _textList = new List<TextObject>();
+        private static Vector2 _startpoint = new Vector2(Application.SCREEN_WIDTH * 0.5f, Application.SCREEN_HEIGHT + 100);
+
+        private float _timer = 0;
+        private const float TIME_BETWEEN_TEXTS = 3;
+        private const float MOVE_SPEED = 75;
 
         public CreditScreen(Game game) : base(game) { }
 
@@ -31,14 +35,22 @@ namespace HypoluxAdventure.Screens
 
         public override void Update(GameTime gameTime)
         {
-            _textList[0] = new TextObject(_titleFont, "Crédits : ", startpoint);
-            //_textList[1] = new TextObject(_normalFont, "Jeu créé par Mathieu 'HazelSoul' ROSTAING, Kévin 'FrancePVP' GRANDJEAN, Noa 'ShakraSasukeXxxD4RK_Sn4keX223xxXNaruto' GUILLOT");
+            //_textList[0] = new TextObject(_titleFont, "Crédits : ", _startpoint);
+            //_textList[1] = new TextObject(_normalFont, "Jeu créé par :\nMathieu 'HazelSoul' ROSTAING\nKévin 'FrancePVP' GRANDJEAN\nNoa 'ShakraSasukeXxxD4RK_Sn4keX223xxXNaruto' GUILLOT", _startpoint2);
+            //_textList[2] = new TextObject(_normalFont, "Level Design fait par :\nMathieu 'HazelSoul' ROSTAING\nKévin 'FrancePVP' GRANDJEAN\nNoa 'ShakraSasukeXxxD4RK_Sn4keX223xxXNaruto' GUILLOT", _startpoint3);
+            //_textList[3] = new TextObject(_normalFont, "Direction artistique et graphismes conçuent par:\nMathieu 'HazelSoul' ROSTAING (carry)\nKévin 'FrancePVP' GRANDJEAN\nNoa 'ShakraSasukeXxxD4RK_Sn4keX223xxXNaruto' GUILLOT", _startpoint4);
+            //_textList[4] = new TextObject(_normalFont, "Sound Design ")
         }
 
         public override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
 
+        }
+
+        public void AddLines(SpriteFont font, params string[] lines)
+        {
+            foreach (string line in lines) _textList.Add(new TextObject(font, line, _startpoint));
         }
         
     }
