3c50cf29d9130409596fb6c50b8e7a4fe5262cf4
Noa GUILLOT <noa.guillot@etu.univ-smb.fr>
2023-01-04 10:35:59 +0100

    Ajout de font et création de TextObject

---
+++ Projet/Screens/CreditScreen.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.Screens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HypoluxAdventure.Screens
+{
+    internal class CreditScreen : GameScreen
+    {
+        private SpriteFont _titleFont;
+        private SpriteFont _normalFont;
+
+        public CreditScreen(Game game) : base(game) { }
+
+        public override void LoadContent()
+        {
+            _titleFont = Content.Load<SpriteFont>("Font/TitleCredit.spritefont");
+            _normalFont = Content.Load<SpriteFont>("Font/CreditFont.spritefont");
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            _titleFont.MeasureString()
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+        }
+        
+    }
+}
