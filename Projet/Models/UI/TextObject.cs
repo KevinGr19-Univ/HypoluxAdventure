817387dfca9ee0db4a285b7ad3815cac2e101ae8
Noa GUILLOT <noa.guillot@etu.univ-smb.fr>
2023-01-04 10:41:06 +0100

    Classe TextObject

--- Projet/Models/UI/TextObject.cs
+++ Projet/Models/UI/TextObject.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,16 @@ namespace HypoluxAdventure.Models.UI
 {
     internal class TextObject
     {
+        private SpriteFont font;
+        private string text;
 
+        public TextObject(SpriteFont font, string text)
+        {
+            this.font = font;
+            this.text = text;
+        }
+
+        public SpriteFont Font { get => font; set => font = value; }
+        public string Text { get => text; set => text = value; }
     }
 }
