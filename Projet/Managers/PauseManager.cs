a5e19406101690713e581664a5da1c9c9b338883
Noa GUILLOT <noa.guillot@etu.univ-smb.fr>
2023-01-06 00:11:08 +0100

    PauseManager rect

--- Projet/Managers/PauseManager.cs
+++ Projet/Managers/PauseManager.cs
@@ -1,23 +1,30 @@
 ﻿using HypoluxAdventure.Models;
 using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HypoluxAdventure.Core;
 
 namespace HypoluxAdventure.Managers
 {
     internal class PauseManager : GameObject
     {
+        private RectangleF _rect;
+
         public PauseManager(Game1 game, GameManager gameManager) : base(game, gameManager) { }
 
         public override void Draw()
         {
-            
+            game.UICanvas.FillRectangle(_rect, Color.Black*0.3f);
         }
 
         public override void Update()
         {
+            _rect = new RectangleF(0, 0, Application.SCREEN_WIDTH, Application.SCREEN_HEIGHT);
             
         }
     }
