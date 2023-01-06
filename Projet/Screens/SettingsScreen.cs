ff2c53ae0ef5322f8334327cee447b033901fc7c
HazelSoul <Mathieu.Rostaing@etu.univ-smb.fr>
2023-01-06 17:57:04 +0100

    generate settings'screen fil

---
+++ Projet/Screens/SettingsScreen.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HypoluxAdventure.Screens
+{
+    internal class SettingsScreen : AbstractScreen
+    {
+        public SettingsScreen(Game1 game) : base(game)
+        {
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            
+        }
+    }
+}
