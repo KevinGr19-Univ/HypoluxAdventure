df8ae62d84ace001ad2948da5f0b1ade2925c4b1
HazelSoul <mathieu.rostaing@etu.univ-smb.fr>
2023-01-07 10:37:50 +0100

    image display settings screen

--- Projet/Screens/SettingsScreen.cs
+++ Projet/Screens/SettingsScreen.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using HypoluxAdventure.Core;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended.Sprites;
 using System;
 using System.Collections.Generic;
@@ -10,16 +12,29 @@ namespace HypoluxAdventure.Screens
 {
     internal class SettingsScreen : AbstractScreen
     {
+        private static Color BACKGROUND_COLOUR = new Color(30,30,30);
 
         private Sprite _titleSprite;
+        private Vector2 _titlePosition;
+        private float _titleRotation;
+        private Vector2 _titleScale;
         public SettingsScreen(Game1 game) : base(game)
         {
 
         }
 
+        public override void LoadContent()
+        {
+            _titleSprite = new Sprite(Content.Load<Texture2D>("img/titleBackgroundImage"));
+            _titlePosition = new Vector2(175,100);
+            _titleRotation = 0;
+            _titleScale = Vector2.One * 0.3f;
+        }
+
         public override void Draw(GameTime gameTime)
         {
-            
+            GraphicsDevice.Clear(SettingsScreen.BACKGROUND_COLOUR);
+            _titleSprite.Draw(Game.UICanvas, _titlePosition, _titleRotation, _titleScale);
         }
 
         public override void Update(GameTime gameTime)
