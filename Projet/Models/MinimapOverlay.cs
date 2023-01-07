aa8c9292f4c68d906fb2ceb0d9f7ec654e3821e4
KevinGr19 <kevingr19dev@gmail.com>
2023-01-07 15:21:21 +0100

    Correctif Minimap

--- Projet/Models/MinimapOverlay.cs
+++ Projet/Models/MinimapOverlay.cs
@@ -20,20 +20,19 @@ namespace HypoluxAdventure.Models
         private const float INVERSE_MAP_WIDTH = 1f / RoomManager.MAP_ROOM_WIDTH;
         private const float TILE_RATIO = ROOM_RECT_WIDTH * INVERSE_MAP_WIDTH;
 
-        private static readonly Color BG_COLOR = new Color(Color.Black, 0.7f);
+        private static readonly Color BG_COLOR = new Color(Color.Black, 0.5f);
         private static readonly Color ROOM_COLOR = Color.White;
         private static readonly Color CURRENT_ROOM_COLOR = Color.Red;
 
         private RectangleF _background;
-        private RectangleF _roomRect;
+        private Vector2 _roomRectTopLeft;
 
         private List<Point> _visitedRooms = new List<Point>();
 
         public MinimapOverlay(Game1 game, GameManager gameManager) : base(game, gameManager)
         {
             _background = new RectangleF(25, 25, WIDTH, WIDTH);
-            _roomRect = new RectangleF(_background.TopLeft + new Vector2(WIDTH - ROOM_RECT_WIDTH) * 0.5f,
-                new Vector2(ROOM_RECT_WIDTH));
+            _roomRectTopLeft = _background.TopLeft + new Vector2(WIDTH - ROOM_RECT_WIDTH) * 0.5f;
         }
 
         public override void Update() { }
@@ -44,7 +43,7 @@ namespace HypoluxAdventure.Models
 
             foreach(Point roomPos in _visitedRooms)
             {
-                Vector2 topLeft = _roomRect.TopLeft + roomPos.ToVector2() * TILE_RATIO;
+                Vector2 topLeft = _roomRectTopLeft + roomPos.ToVector2() * TILE_RATIO;
                 RectangleF rect = new RectangleF(topLeft, new Vector2(TILE_RATIO));
                 rect = rect.Scale(new Vector2(ROOM_TILE_SCALE));
 
