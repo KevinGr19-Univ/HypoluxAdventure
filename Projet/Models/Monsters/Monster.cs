490ab5ea2f50d4cf17a6044a4fc09ef1a63e6358
KevinGr19 <kevingr19dev@gmail.com>
2023-01-08 13:08:43 +0100

    Globux IA

--- Projet/Models/Monsters/Monster.cs
+++ Projet/Models/Monsters/Monster.cs
@@ -1,9 +1,11 @@
-﻿using HypoluxAdventure.Managers;
+﻿using HypoluxAdventure.Core;
+using HypoluxAdventure.Managers;
 using HypoluxAdventure.Models.Rooms;
 using Microsoft.Xna.Framework;
 using MonoGame.Extended.Sprites;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +14,7 @@ namespace HypoluxAdventure.Models.Monsters
 {
     internal abstract class Monster : Entity
     {
-        protected AnimatedSprite AnimatedSprite => (AnimatedSprite)base.Sprite;
+        protected AnimatedSprite AnimatedSprite => (AnimatedSprite)Sprite;
         protected Room room;
 
         /// <summary>The spawn position of the monster, relative to the room.</summary>
@@ -27,7 +29,77 @@ namespace HypoluxAdventure.Models.Monsters
             spawnPosition = spawnPos;
         }
 
+        protected bool overlapsPlayer;
+
+        public bool IsPlayerInRange(float range) =>
+            (Position - gameManager.Player.Position).LengthSquared() < range * range;
+
+        public bool CanSeePlayer(float range)
+        {
+            const int POINTS_AMOUNT = 10;
+            if (!IsPlayerInRange(range)) return false;
+
+            for(int i = 1; i <= POINTS_AMOUNT; i++)
+            {
+                Vector2 lerpPoint = Vector2.Lerp(Position, gameManager.Player.Position, i / POINTS_AMOUNT);
+                Point tilePos = ((lerpPoint - room.Position) / Room.TILE_SIZE).ToPoint();
+
+                if (room.IsWall(tilePos.X, tilePos.Y))
+                {
+                    Logger.Debug($"Is wall {i} : {tilePos}");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public Vector2 TowardsPlayer()
+        {
+            if (gameManager.Player.Position == Position) return Vector2.Zero;
+            return Vector2.Normalize(gameManager.Player.Position - Position);
+        }
+
+        #region Damage Red Pulse
+        private const float PULSE_TIME = 0.4f;
+        private float _pulseTimer;
+
+        public override void OnDamage()
+        {
+            base.OnDamage();
+            _pulseTimer = PULSE_TIME;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            AnimatedSprite.Update(Time.DeltaTime);
+
+            if (!IsDead) Move();
+
+            overlapsPlayer = Hitbox.Intersects(gameManager.Player.Hitbox);
+            if (overlapsPlayer) OnPlayerCollision();
+
+            if(_pulseTimer > 0) _pulseTimer -= Time.DeltaTime;
+        }
+
+        public override void Draw()
+        {
+            Sprite.Color = _pulseTimer > 0 ? Color.Lerp(Color.White, Color.Red, _pulseTimer / PULSE_TIME) : Color.White;
+            base.Draw();
+        }
+        #endregion
+
         abstract public void OnPlayerCollision();
-        abstract public void Spawn();
+        public virtual void Spawn()
+        {
+            Health = MaxHealth;
+            Position = spawnPosition;
+        }
+
+        public virtual void Unload()
+        {
+            if (IsDead) IsSlained = true;
+        }
     }
 }
